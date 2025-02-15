4cdf161addd9e42b53b7fd57e9edfe4260206633
floweraviles <itsfloweraviles@gmail.com>
2025-02-15 10:53:35 -0500

    made express app entry point

---
+++ backend/src/server.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const { sequelize } = require("./models");
+const authRoutes = require("./routes/authRoutes");
+const podcastRoutes = require("./routes/podcastRoutes");
+const scriptRoutes = require("./routes/scriptRoutes");
+
+const app = express();
+
+//Middleware 
+app.use(cors());
+app.use(express.json());
+
+//Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/podcast", podcastRoutes);
+app.use("api/scripts", scriptRoutes);
+
+//Start Server
+const PORT = process.env.PORT || 5000;
+sequelize.sync().then(() => {
+    console.log("Database connected.");
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+});
