bd384b433a46714197e6ff94ce971937072b21ec
floweraviles <itsfloweraviles@gmail.com>
2025-02-25 13:58:32 -0500

    update tailwind file

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+    content: ["./index.html", "./src/**/*.{js, jsx}"],
+    theme: {
+        extend: {
+            colors: {
+                cyberBlack: "#0d0d0d",
+                neonBlue: "#00eaff",
+                neonPurple: "#a200ff",
+                neonPink: "ff007f"
+            },
+        },
+    },
+    plugins: [],
+};
