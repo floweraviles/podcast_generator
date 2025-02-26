6adb8896dfb083dbf1f81c768ba4494434d80cef
floweraviles <itsfloweraviles@gmail.com>
2025-02-25 20:30:10 -0500

    created login page

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import { useAuth } from "../hooks/useAuth";
+import { useNavigate } from "react-router-dom";
+
+const Login = () => {
+    const [email, setemail] = useState("");
+    const [password, setPassword] = useState("");
+    const { login } = useAuth();
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const success = await login(email, password);
+        if (success) navigate("/dashboard");
+    };
+    return (
+        <div>
+            <h2>Login</h2>
+            <form>
+                <input type="email" placeholder="Email" value={email} onChange={(e) => setemail(e.target.value)} required />
+                <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    )
+}
