f11c35cf6fe3d101d1b8a3dbcc7fc94f21c10e9d
floweraviles <itsfloweraviles@gmail.com>
2025-02-25 21:47:41 -0500

    created user registration

---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const Register = () => {
+    const [formData, setFormData] = useState({ name: "", email: "", password: "" });
+    const [error, setError] = useState("");
+    const navigate = useNavigate();
+
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.taget.value });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setError("");
+
+        try {
+            const response = await fetch(`${import.meta.env.VITE_API_BASE_URL}/auth/register`, {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(formData),
+            });
+            const data = await response.json();
+            if (!response.ok) throw new Error(data.error || "Registration failed");
+
+            navigate("/login");
+        } catch (err) {
+            setError(err.message);
+        }
+    };
+
+    return (
+        <div>
+            <h2>Create an Account</h2>
+            {error && <p>{error}</p>}
+
+            <form>
+                <input type="text" name="name" placeholder="Name" onChange={handleChange} required />
+                <input type="email" name="email" placeholder="Email" onChange={handleChange} required />
+                <input type="password" name="password" placeholder="Password" onChange={handleChange} required />
+                <button>Register</button>
+            </form>
+        </div>
+    )
+
+}
