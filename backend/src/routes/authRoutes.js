99505b83bc3cd8578bd0b2bd7714ce0fef777ef7
floweraviles <itsfloweraviles@gmail.com>
2025-04-09 14:11:34 -0400

    applied validation to auth routes

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const { register, login } = require("../controllers/authController");
+const { validateRegister, validateLogin } = require("../middleware/validators");
+const handleValidation = require("../middleware/handleValidation");
 
 const router = express.Router();
 
-router.post("/register", register);
-router.post("/login", login);
+router.post("/register", validateRegister, handleValidation, register);
+router.post("/login",  validateLogin, handleValidation, login);
 
 module.exports = router;
