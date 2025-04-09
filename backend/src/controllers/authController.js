f014cd47b27fe99d642833a6c877d81c51f7a7ea
floweraviles <itsfloweraviles@gmail.com>
2025-04-08 21:50:56 -0400

    added email format check, password strength check, and check for existing user.

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -2,9 +2,37 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const { User } = require("../models");
 
+const isStrongPassword = (password) => {
+    return (
+        password.length >= 10 &&
+        /[A-Z]/.test(password) &&
+        /[a-z]/.test(password) &&
+        /[0-9]/.test(password) &&
+        /[^A-Za-z0-9]/.test(password)
+    );
+};
+
 exports.register = async (req, res) => {
     try {
         const { email, password } = req.body;
+        if (!email || !password) {
+            return res.status(400).json({ error: "Email and password are required."});
+        }
+        const emailRegex = /^[^\s@]+[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email)) {
+            return  res.status(400).json({ error: "Invalid email format."});
+        }
+
+        if (!isStrongPassword(password)) {
+            return res.status(400).json({
+                error: "Password must be at 10 characters long and include uppercase, lowercase, number, and special character.",
+            });
+        }
+
+        const existingUser = await User.findOne({ where: { email } });
+        if (existingUser) {
+            return res.status(409).json({ error: "Email already registered."});
+        }
         const hashedPassword = await bcrypt.hash(password, 10);
         const user = await User.create({ email, password: hashedPassword });
         res.status(201).json({ message: "User registered successfully" });
